77e77af5686e23d6b3d3fa9e930c2aa8bf4d0c8a
Jayesh Prajapati <156836276+Jayesh-Prajapati9@users.noreply.github.com>
2025-01-20 09:56:25 +0530

    ToDos Route Updated

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 let port = 8080;
+app.use(express.json);
 
-app.get('/', (res, req) => {
-    res.send("Helloo World");       
+app.get('/singup', (res, req) => {
 });
 
+app.post('\todos',(req,res)=>{
+    const token = req.body.token;
+    const verification= jwt.verify(token,JWT_SECRET);
+    const email = verification.email;
+
+    const UserTodos = UserModal.findOne({
+        email: email;
+    })
+});
